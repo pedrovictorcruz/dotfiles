808114addbdf68615cab530171cf383333d51473
Pedro Cruz <pedrovictor.lage@gmail.com>
2024-07-09 17:58:33 -0300

    [nvim] add keymap to copy selected text to clipboard

--- nvim/lua/pedrocruz/remap.lua
+++ nvim/lua/pedrocruz/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
