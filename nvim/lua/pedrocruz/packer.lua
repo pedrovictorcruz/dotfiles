975240cb2eff3613e0a60cdabebe5a908898a7e2
Pedro Cruz <pedrovictor.lage@gmail.com>
2024-07-12 15:11:31 -0300

    [nvim] add obsidian

--- nvim/lua/pedrocruz/packer.lua
+++ nvim/lua/pedrocruz/packer.lua
@@ -1,45 +1,75 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    use("wbthomason/packer.nvim")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.6",
         -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-    use('mbbill/undotree')
+    use("mbbill/undotree")
 
-    use('m6vrm/gruber.vim')
+    use("m6vrm/gruber.vim")
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v1.x",
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },             -- Required
-            { 'williamboman/mason.nvim' },           -- Optional
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            { "neovim/nvim-lspconfig" },             -- Required
+            { "williamboman/mason.nvim" },           -- Optional
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },         -- Required
-            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-            { 'hrsh7th/cmp-buffer' },       -- Optional
-            { 'hrsh7th/cmp-path' },         -- Optional
-            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+            { "hrsh7th/nvim-cmp" },         -- Required
+            { "hrsh7th/cmp-nvim-lsp" },     -- Required
+            { "hrsh7th/cmp-buffer" },       -- Optional
+            { "hrsh7th/cmp-path" },         -- Optional
+            { "saadparwaiz1/cmp_luasnip" }, -- Optional
+            { "hrsh7th/cmp-nvim-lua" },     -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },             -- Required
-            { 'rafamadriz/friendly-snippets' }, -- Optional
-        }
-    }
+            { "L3MON4D3/LuaSnip" },             -- Required
+            { "rafamadriz/friendly-snippets" }, -- Optional
+        },
+    })
 
-    use('nvim-tree/nvim-web-devicons')
-    use('lewis6991/gitsigns.nvim')
-    use('romgrk/barbar.nvim')
+    use("nvim-tree/nvim-web-devicons")
+    use("lewis6991/gitsigns.nvim")
+    use("romgrk/barbar.nvim")
+
+    use({
+        "epwalsh/obsidian.nvim",
+        tag = "*", -- recommended, use latest release instead of latest commit
+        requires = {
+            -- Required.
+            "nvim-lua/plenary.nvim",
+
+            -- see below for full list of optional dependencies 👇
+        },
+        config = function()
+            require("obsidian").setup({
+                ui = {
+                    enable = false
+                },
+                workspaces = {
+                    {
+                        name = "personal",
+                        path = "~/vaults/personal",
+                    },
+                    {
+                        name = "work",
+                        path = "~/vaults/work",
+                    },
+                },
+
+            })
+        end,
+    })
 end)
--- nvim/lua/pedrocruz/set.lua
+++ nvim/lua/pedrocruz/set.lua
@@ -31,5 +31,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
-vim.cmd('hi! link SignColumn Normal')
