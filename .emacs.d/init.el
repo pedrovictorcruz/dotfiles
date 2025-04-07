6bc571c015534993ce9790b9483630b3a068828f
Pedro Cruz <pedrovictor.lage@gmail.com>
2025-04-07 09:25:28 -0300

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,10 +3,10 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-(set-frame-font "FiraMono Nerd Font 20" nil t)
+(set-frame-font "BlexMono Nerd Font 20" nil t)
 
 ;; Diretório padrão
-(setq default-directory "~/development")
+(setq default-directory "d:/Development")
 
 ;; Backups e avisos
 (setq make-backup-files nil)
@@ -18,9 +18,6 @@
 (setq display-line-numbers 'relative)
 (global-auto-revert-mode 1)
 (global-subword-mode 1)
-
-(setq shell-file-name "/usr/bin/bash")
-(setenv "SHELL" shell-file-name)
 
 ;; CUA mode
 (cua-mode 1)
@@ -37,7 +34,6 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; use-package bootstrap
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 (require 'use-package)
@@ -65,32 +61,19 @@
     :global-prefix "C-SPC")
 
   (my/leader
+    "f"  '(:ignore t :which-key "files")
     "f d" 'find-file
-    "f f" 'lsp-format-buffer
+    "f f" 'formatting-buffer
     "b"  'switch-to-buffer
     "k"  'kill-buffer
     "g"  '(:ignore t :which-key "git")
     "g s" 'magit-status
-    "q"  'save-buffers-kill-terminal
-    "K"  'lsp-ui-doc-show
-    "r n" 'lsp-rename
-    "g d" 'lsp-find-definition
-    "g r" 'lsp-find-references))
+    "q"  'save-buffers-kill-terminal))
 
-;; IDO
-(use-package ido
-  :init
-  (setq ido-enable-flex-matching t
-        ido-create-new-buffer 'always
-        confirm-nonexistent-file-or-buffer nil
-        ido-ignore-buffers '("\\` " "^\\*"))
-  :config
-  (ido-mode 1)
-  (ido-everywhere 1)
-  (global-set-key (kbd "C-x b") 'ido-switch-buffer))
-
-(setq ido-create-new-buffer 'always)
-(setq confirm-nonexistent-file-or-buffer nil)
+;; IDO mode
+(ido-mode 1)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
 
 ;; Temas
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
@@ -106,46 +89,28 @@
   (setq magit-status-buffer-switch-function 'switch-to-buffer
         magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
-;; JSON
-(use-package json-mode)
+;; Auto-refresh dired on file change
+(add-hook 'dired-mode-hook 'auto-revert-mode)
 
 ;; Reformatter (formatting.el usa isso)
 (use-package reformatter)
 
-;; LSP
-(use-package lsp-mode
-  :hook ((typescript-mode . lsp)
-         (kotlin-mode . lsp)
-         (c-mode . lsp)
-         (c++-mode . lsp)
-         (csharp-mode . lsp))
-  :commands lsp
+;; Tree-sitter
+(use-package treesit-auto
   :config
-  (setq lsp-enable-snippet nil))
+  (setq treesit-auto-install 'prompt)
+  (global-treesit-auto-mode))
 
-(use-package lsp-ui
-  :commands lsp-ui-mode)
+(setq major-mode-remap-alist
+      '((c-mode          . c-ts-mode)
+        (c++-mode        . c++-ts-mode)
+        (json-mode       . json-ts-mode)
+        (js-mode         . js-ts-mode)
+        (typescript-mode . tsx-ts-mode)))
 
-;; Linguagens
-(use-package typescript-mode
-  :mode ("\\.ts\\'" "\\.tsx\\'" "\\.js\\'" "\\.jsx\\'"))
-
-(use-package kotlin-mode
-  :mode ("\\.kts?\\'")
-  :config
-  (setq kotlin-tab-width 4))
-
+;; Simpc
 (require 'simpc-mode)
 (add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
-
-(use-package csharp-mode
-  :mode ("\\.cs\\'" . csharp-mode))
-
-;; Move lines
-(defun move-line-up () (interactive) (transpose-lines 1) (previous-line 2))
-(defun move-line-down () (interactive) (next-line 1) (transpose-lines 1) (previous-line 1))
-(global-set-key (kbd "M-p") 'move-line-up)
-(global-set-key (kbd "M-n") 'move-line-down)
 
 (define-key key-translation-map (kbd "C-c") (kbd "<escape>"))
 (define-key evil-normal-state-map (kbd "C-r") 'evil-redo)
@@ -154,5 +119,16 @@
 (require 'formatting)
 
 (custom-set-variables
- '(package-selected-packages nil))
-(custom-set-faces)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-collection general json-mode kotlin-mode lsp-ui magit
+		     reformatter treesit-auto typescript-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
