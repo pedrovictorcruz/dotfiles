eb3e3876f93d9c3d14637984da1acc2e7c1e21d2
Pedro Cruz <pedrovictor.lage@gmail.com>
2025-11-12 11:53:12 -0300

    [nvim] add native lsp completion

--- nvim/lua/pedro/lsp.lua
+++ nvim/lua/pedro/lsp.lua
@@ -6,6 +6,19 @@ vim.lsp.enable({
 	"jsonls",
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      vim.keymap.set('i', '<C-Space>', function()
+        vim.lsp.completion.get()
+      end)
+    end
+  end,
+})
+
 vim.diagnostic.config({
 	virtual_text = true,
 	virtual_lines = false,
