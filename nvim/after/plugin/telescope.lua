4a8da7fe17434d3af7c4ae4fd97f73e5e0e7927c
Pedro Cruz <pedrovictor.lage@gmail.com>
2026-02-10 09:49:13 -0300

    add lsp key

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -19,6 +19,9 @@ require('telescope').setup({
 
 vim.keymap.set('n', '<leader>fd', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set("n", "<leader>ls", function()
+  builtin.lsp_document_symbols()
+end, { })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>en', function ()
     builtin.find_files {
